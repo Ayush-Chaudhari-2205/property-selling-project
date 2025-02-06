1c21f18e57b65ac8211a3f630e23cde5318d688d
Piyush Patil <piyushpatil0310@gmail.com>
2025-02-06 10:39:34 +0530

    solving merge

--- property-selling-fontend/src/components/NavBar.jsx
+++ property-selling-fontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
   return (
      <nav className="navbar navbar-expand-lg fixed-top bg-body clean-navbar">
-        <div className="container"><a className="navbar-brand logo" href="#">RadientProperty</a><button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+        <div className="container"><a className="navbar-brand logo" href="#">RadiantProperty</a><button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navcol-1">
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item"><a className="nav-link active" href="/index.html">Home</a></li>
